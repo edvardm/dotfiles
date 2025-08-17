453b753d0794e680e70d20ab3da8283f522c3a5f
Edvard Majakari <edvard@majakari.net>
2025-08-17 12:06:14 +0300

    snapshot

--- .config/lazynvim/lua/plugins/example.lua
+++ .config/lazynvim/lua/plugins/example.lua
@@ -1,7 +1,9 @@
 -- since this is just an example spec, don't actually load anything here and return an empty spec
--- stylua: ignore
-if true then return {} end
+if true then
+  return {}
+end
 
+-- stylua: ignore
 -- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
 --
 -- In your plugin files, you can:
