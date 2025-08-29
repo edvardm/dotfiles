508971646f627f4766e3abce43e805bf07343e76
Edvard Majakari <edvard@majakari.net>
2025-08-29 18:13:49 +0300

    handle bg outside repo, setup mouse copy/paste

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,19 +1,19 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
-local dimmer = { brightness = 0.1 }
+local dimmer = { brightness = 0.6 }
 config.background = {
-    {
-        source = {
-            File=os.getenv("HOME") .. "/Pictures/spaceship-warp.png",
-        },
-        attachment = "Fixed",
-        repeat_x = "NoRepeat",
-        repeat_y = "NoRepeat",
-        vertical_align = 'Middle',
-        opacity = 0.95,
-        hsb = dimmer,
-    }
+	{
+		source = {
+			File = os.getenv("HOME") .. "/Pictures/wezterm-bg.jpg",
+		},
+		attachment = "Fixed",
+		repeat_x = "NoRepeat",
+		repeat_y = "NoRepeat",
+		vertical_align = "Middle",
+		opacity = 0.97,
+		hsb = dimmer,
+	},
 }
 
 config.text_background_opacity = 0.7
@@ -25,5 +25,20 @@ config.font_size = 13
 -- config.color_scheme = 'AdventureTime'
 
 config.audible_bell = "Disabled"
+config.mouse_bindings = {
+	{
+		event = { Down = { streak = 1, button = "Right" } },
+		mods = "NONE",
+		action = wezterm.action_callback(function(window, pane)
+			local has_selection = window:get_selection_text_for_pane(pane) ~= ""
+			if has_selection then
+				window:perform_action(act.CopyTo("ClipboardAndPrimarySelection"), pane)
+				window:perform_action(act.ClearSelection, pane)
+			else
+				window:perform_action(act({ PasteFrom = "Clipboard" }), pane)
+			end
+		end),
+	},
+}
 
 return config
